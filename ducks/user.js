68249de77c73fc0f13ae9df85f2ba3e7a24bd58f
Jordan Buon <jordanmb803@github.com>
2018-05-22 10:53:49 -0600

    getUser action created

--- ducks/user.js
+++ ducks/user.js
@@ -1,8 +1,34 @@
+import axios from 'axios'
+
 const initialState = {
     user: {}
 }
 
 
-export default function reducer (state=initialState, action){
-    return state
+//Action Types
+const GET_USER = 'GET_USER'
+
+//MiddleWare
+const _FULLFILLED = '_FULLFILLED'
+
+export function getUser() {
+    let userData = app.get('/auth/me').then(res => {
+        return res.data
+    })
+    return {
+        type: GET_USER,
+        payload: userData
+    }
+}
+
+
+
+export default function reducer(state = initialState, action) {
+    switch(action.type){
+        case GET_USER + _FULLFILLED:
+            return Object.assign({}, state, {user: action.payload})
+   
+        default:
+            return state
+    }
 }
