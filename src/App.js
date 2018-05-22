f91afb76f9304cb9c32922058e97f11d60137f8b
Jordan Buon <jordanmb803@github.com>
2018-05-22 11:02:50 -0600

    changed first route to DailyView

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 
+import DailyView from './components/DailyView/DailyView';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <HashRouter>
           <Switch>
-            <Route />
+            <Route path='/' component={DailyView}/>
           </Switch>
         </HashRouter>
       </div>
