399a04bb90451efa574c31cc2e164bd1b154b11a
Jordan Buon <jordanmb803@github.com>
2018-05-22 11:01:29 -0600

    HashRouter Set Up and 1st Route created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-     
+        <HashRouter>
+          <Switch>
+            <Route />
+          </Switch>
+        </HashRouter>
       </div>
     );
   }
---
+++ src/components/DailyView/DailyView.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class DailyView extends Component {
+    render(){
+        return(
+            <div>DailyView</div>
+        )
+    }
+}
