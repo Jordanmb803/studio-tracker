2fb446921af290e34f25627d2155563d9d87f0ff
Jordan Buon <jordanmb803@github.com>
2018-05-23 14:06:45 -0600

    menu drop down logo imported

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import menu from './arq-menu-512.png';
 import './Nav.css'
+
 class Nav extends Component {
     constructor() {
         super()
@@ -16,7 +18,7 @@ class Nav extends Component {
                     <img id='profileImg' src={this.props.user.profile_picture} alt='prof_pic' />
                     <h1>Studio Tracker</h1>
                 </div>
-                <button>tab bar</button>
+                    <img className='dropDown' src={menu}/>
             </div>
         )
     }
