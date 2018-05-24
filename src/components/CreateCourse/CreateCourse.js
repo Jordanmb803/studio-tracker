a4026031f69d2966ca5606749d05357a71df13fc
Jordan Buon <jordanmb803@github.com>
2018-05-24 17:45:47 -0600

    inputs connected to state

--- src/components/CreateCourse/CreateCourse.js
+++ src/components/CreateCourse/CreateCourse.js
@@ -7,22 +7,24 @@ class CreateCourse extends Component {
         this.state = {
             classNumber: 0,
             classTitle: '',
-            classLength: 0,
+            length: 0,
             dayOfWeek: '',
             time: '',
             teacherName: '',
         }
     }
+
+
     render() {
         return (
             <div className='createCourse'>
                 <h3 className='createCourseHeader'>Create A New Class</h3>
-                <input placeholder='Class Number' className='courseInput' />
-                <input placeholder='Class Title' className='courseInput' />
-                <input placeholder='Class length in minutes' className='courseInput' />
-                <input placeholder='Day of the Week, Options: S, M, T, W, TH, F, SAT' className='courseInput' />
-                <input placeholder='Time, Ex: 7pm' className='courseInput' />
-                <input placeholder='Teacher Name' className='courseInput' />
+                <input placeholder='Class Number' className='courseInput' onChange={e => this.setState({ classNumber: e.target.value})}/>
+                <input placeholder='Class Title' className='courseInput'  onChange={e => this.setState({ classTitle: e.target.value})}/>
+                <input placeholder='Class length in minutes' className='courseInput'  onChange={e => this.setState({ length: e.target.value})}/>
+                <input placeholder='Day of the Week, Options: S, M, T, W, TH, F, SAT' className='courseInput'  onChange={e => this.setState({ dayOfWeek: e.target.value})}/>
+                <input placeholder='Time, Ex: 7pm' className='courseInput'  onChange={e => this.setState({ time: e.target.value})}/>
+                <input placeholder='Teacher Name' className='courseInput'  onChange={e => this.setState({ teacherName: e.target.value})}/>
                 <button className='createCourseButton'>Create New Class</button>
             </div>
         )
