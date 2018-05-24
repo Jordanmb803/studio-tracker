39a8d7d193ac31954d3303f75911d6e7f475b3b3
Jordan Buon <jordanmb803@github.com>
2018-05-24 16:14:11 -0600

    admin landing styles added

--- src/components/AdminLanding/AdminLanding.js
+++ src/components/AdminLanding/AdminLanding.js
@@ -6,10 +6,10 @@ class AdminLanding extends Component {
     render(){
         return(
             <div className='adminLanding'>
-                <Link to='/trackhours'><button className='adminOption'>Track Hours</button></Link>
-                <Link to='/trackattendance'><button className='adminOption'>Track Attendance</button></Link>
-                <Link to='/createcourse'><button className='adminOption'>Create Class</button></Link>
-                <Link to='/registeruser'><button className='adminOption'>Register Teacher/Student</button></Link>
+                <Link className='adminLinks' to='/trackhours'><button className='adminOption'>Track Hours</button></Link>
+                <Link className='adminLinks' to='/trackattendance'><button className='adminOption'>Track Attendance</button></Link>
+                <Link className='adminLinks' to='/createcourse'><button className='adminOption'>Create Class</button></Link>
+                <Link className='adminLinks' to='/registeruser'><button className='adminOption'>Register Teacher/Student</button></Link>
             </div>
         )
     }
