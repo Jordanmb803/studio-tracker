51596e7e8d2c5932829f718bfd86544b0b05d0da
Jordan Buon <jordanmb803@github.com>
2018-05-24 09:00:35 -0600

    sample data created

--- db/seed.sql
+++ db/seed.sql
@@ -7,7 +7,9 @@ CREATE TABLE user (
     city varchar(75),
     state varchar(75),
     zipcode varchar(15),
-    type varchar(30)
+    type varchar(30),
+    user_name text,
+    profile_picture varchar(1000)
 );
 
 CREATE TABLE class (
@@ -17,6 +19,7 @@ CREATE TABLE class (
     ,length integer
     ,day varchar(15)
     ,time text
+    ,teacher_id
 
 )
 
@@ -30,3 +33,39 @@ INSERT INTO class
 (class_num, title, length, day, time, teacher_id)
 VALUES
 (2, 'Ballet', 60, 'T', '7pm', 1);
+
+INSERT INTO users
+(type, user_name, profile_picture)
+VALUES
+('student', 'Minney Mouse', 'https://lh5.googleusercontent.com/-YqvmY2WsEbg/AAAAAAAAAAI/AAAAAAAAAKA/1x708KmUpp8/photo.jpg')
+,('student', 'James Bond', 'https://lh5.googleusercontent.com/-YqvmY2WsEbg/AAAAAAAAAAI/AAAAAAAAAKA/1x708KmUpp8/photo.jpg')
+,('student', 'Doug Lace', 'https://lh5.googleusercontent.com/-YqvmY2WsEbg/AAAAAAAAAAI/AAAAAAAAAKA/1x708KmUpp8/photo.jpg')
+,('student', 'Drogo Khal', 'https://lh5.googleusercontent.com/-YqvmY2WsEbg/AAAAAAAAAAI/AAAAAAAAAKA/1x708KmUpp8/photo.jpg')
+,('student', 'Barney Fife', 'https://lh5.googleusercontent.com/-YqvmY2WsEbg/AAAAAAAAAAI/AAAAAAAAAKA/1x708KmUpp8/photo.jpg')
+,('student', 'Monster Inc', 'https://lh5.googleusercontent.com/-YqvmY2WsEbg/AAAAAAAAAAI/AAAAAAAAAKA/1x708KmUpp8/photo.jpg');
+
+
+INSERT INTO register
+(user_id, class_id)
+VALUES
+(3, 4)
+,(4, 4)
+,(9, 4)
+,(5, 3)
+,(6, 2)
+,(3, 1)
+,(7, 3)
+,(8, 1)
+,(8, 4)
+,(9, 2);
+
+
+
+
+
+
+
+
+
+
+
