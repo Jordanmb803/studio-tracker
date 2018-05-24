107d4e7aa9101f3d5c22807f8544305543dc7bc6
Jordan Buon <jordanmb803@github.com>
2018-05-24 09:04:51 -0600

    changed functional component to class component

--- src/components/CourseList/CourseList.js
+++ src/components/CourseList/CourseList.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './CourseList.css';
 import { connect } from 'react-redux';
 
-function CourseList(props){
-    return(
-        <div className='dailyView'>
-            <h3>{props.match.params.course}</h3>
-            <button className='submitRole'>Submit Role</button>
-        </div>
-    )
+class CourseList extends Component {
+    render() {
+        return (
+            <div className='dailyView'>
+                <h3>{props.match.params.course}</h3>
+                <button className='submitRole'>Submit Role</button>
+            </div>
+        )
+    }
 }
 
-function mapStateToProps(state){
-    return{
+function mapStateToProps(state) {
+    return {
         danceCourses: state.danceCourses
     }
 }
