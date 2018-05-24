caf833d7515771c09d434d43ff75415cba32a2f2
Jordan Buon <jordanmb803@github.com>
2018-05-24 12:18:58 -0600

    input boxes working

--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -5,8 +5,7 @@ class Student extends Component {
     constructor() {
         super()
         this.state = {
-            present: false,
-            absent: true,
+            present: false
         }
         this.postPresent = this.postPresent.bind(this)
         this.deletePresent = this.deletePresent.bind(this)
@@ -15,7 +14,9 @@ class Student extends Component {
     postPresent() {
         const {user_id, class_id, date} = this.props
         axios.post('/inputhours', {user_id, class_id, date}).then(res => {
-
+            this.setState({
+                present: true,
+            })
         })
     }
 
@@ -30,8 +31,20 @@ class Student extends Component {
         return (
             <div>
                 <p>{this.props.user_name}</p>
-                <input type='checkbox' id='present' name='roll' value='present' onClick={() => this.postPresent()} />
-                <input type='checkbox' id='absent' name='roll' value='absent' onClick={() => this.deletePresent()} />
+                <input type='checkbox' id='present' name='roll' value='present' onClick={() => this.postPresent()} checked={this.state.present} onChange={(e)=> {
+
+                    e.target.checked = this.state.present
+                    this.setState({
+                        present: !this.state.present
+                    })
+                }
+                }/>
+                <input type='checkbox' id='absent' name='roll' value='absent' onClick={() => this.deletePresent()} checked={!this.state.present} onChange={(e)=> {
+                    e.target.checked = !this.state.present
+                    this.setState({
+                        present: !this.state.present
+                    })
+                }}/>
             </div>
         )
     }
