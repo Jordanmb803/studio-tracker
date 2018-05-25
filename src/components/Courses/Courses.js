a46b5fd77b0857ab9b692ca353bac12bf01d3839
Jordan Buon <jordanmb803@github.com>
2018-05-25 11:43:19 -0600

    inputs changed to select boxes

--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { getCourses } from '../../ducks/user';
+import { connect } from 'react-redux'
 import './Courses.css';
 
 class Courses extends Component {
-    render(){
-        return(
+    constructor() {
+        super()
+        this.state = {
+        }
+    }
+
+    componentDidMount() {
+        this.props.getCourses()
+    }
+
+    render() {
+        return (
             <div className='Courses'>
-               List of Courses here
+            <h2>Center Stage Class List</h2>
+                {this.props.danceCourses.map((course, i) => {
+                    return (
+                        <div key={course + i}>
+                            <h3>{course.title}</h3>
+                        </div>
+                    )
+                })}
                 <Link to='/createcourse'><button>Create New Course</button></Link>
             </div>
         )
     }
 }
 
-export default Courses;
+function mapStateToProps(state) {
+    return {
+        danceCourses: state.danceCourses
+    }
+}
+
+export default connect(mapStateToProps, { getCourses })(Courses);
--- src/components/CreateCourse/CreateCourse.js
+++ src/components/CreateCourse/CreateCourse.js
@@ -18,7 +18,7 @@ class CreateCourse extends Component {
         this.createCourse = this.createCourse.bind(this)
     }
 
-    componentDidMount(){
+    componentDidMount() {
         axios.get('/getallusers').then(res => {
             this.setState({
                 users: res.data
@@ -27,26 +27,72 @@ class CreateCourse extends Component {
     }
 
 
-    createCourse(){
+    createCourse() {
         const classNumber = Number(this.state.classNumber)
         const length = Number(this.state.length)
-        const {classTitle, dayOfWeek, time, teacherName} = this.state
-        axios.post('/createcourse', {classNumber, classTitle, length, dayOfWeek, time, teacherName}).then(()=> {
-          console.log('we made it')
+        const { classTitle, dayOfWeek, time, teacherName } = this.state
+        axios.post('/createcourse', { classNumber, classTitle, length, dayOfWeek, time, teacherName }).then(() => {
+            console.log('we made it')
         })
     }
 
     render() {
+
+        console.log(this.state.length)
+
         return (
             <div className='createCourse'>
                 <h3 className='createCourseHeader'>Create A New Class</h3>
-                <input placeholder='Class Number' className='courseInput' onChange={e => this.setState({ classNumber: e.target.value})}/>
-                <input placeholder='Class Title' className='courseInput'  onChange={e => this.setState({ classTitle: e.target.value})}/>
-                <input placeholder='Class length in minutes' className='courseInput'  onChange={e => this.setState({ length: e.target.value})}/>
-                <input placeholder='Day of the Week, Options: S, M, T, W, TH, F, SAT' className='courseInput'  onChange={e => this.setState({ dayOfWeek: e.target.value})}/>
-                <input placeholder='Time, Ex: 7pm' className='courseInput'  onChange={e => this.setState({ time: e.target.value})}/>
-                <input placeholder='Teacher Name' className='courseInput'  onChange={e => this.setState({ teacherName: e.target.value})}/>
-                <button className='createCourseButton' onClick={()=> this.createCourse()}>Create New Class</button>
+                <input placeholder='Class Number' className='courseInput' onChange={e => this.setState({ classNumber: e.target.value })} />
+                <input placeholder='Class Title' className='courseInput' onChange={e => this.setState({ classTitle: e.target.value })} />
+
+                <select className='courseInput' onChange={e => this.setState({length: e.target.value})} name='length' form='length'>
+                    <option value=''>Select Class Length</option>
+                    <option value='60'>1 Hour</option>
+                    <option value='90'>1.5 Hours</option>
+                </select>
+
+                <select className='courseInput' onChange={e => this.setState({ dayOfWeek: e.target.value })} name='dayOfTheWeek' form='dayOfTheWeek'>
+                    <option value=''>Select Day</option>
+                    <option value='S'>Sunday</option>
+                    <option value='M'>Monday</option>
+                    <option value='T'>Tuesday</option>
+                    <option value='W'>Wednesday</option>
+                    <option value='TH'>Thursday</option>
+                    <option value='F'>Friday</option>
+                    <option value='SAT'>Saturday</option>
+                </select>
+
+                <select className='courseInput' onChange={e => this.setState({ time: e.target.value })} name='time' form='time'>
+                    <option value=''>Select Time</option>
+                    <option value='9am'>9am</option>
+                    <option value='10am'>10am</option>
+                    <option value='11am'>11am</option>
+                    <option value='12am'>12pm</option>
+                    <option value='1pm'>1pm</option>
+                    <option value='2pm'>2pm</option>
+                    <option value='3pm'>3pm</option>
+                    <option value='4pm'>4pm</option>
+                    <option value='5pm'>5pm</option>
+                    <option value='6pm'>6pm</option>
+                    <option value='7pm'>7pm</option>
+                    <option value='8pm'>8pm</option>
+                    <option value='9pm'>9pm</option>
+                </select>
+                <p>Select A Teacher</p>
+                <div className='teachersDiv'>
+                    {this.state.users.filter(user => {
+                        return user.type === 'teacher'
+                    }).map((teacher, i) => {
+                        return (<div key={i + teacher} >
+                            <img src={teacher.profile_picture} alt={teacher} className='teacherPics' onClick={() => this.setState({ teacherName: teacher.user_name })} />
+                            <p className='teacherName'>{teacher.user_name}</p>
+                        </div>
+                        )
+                    })
+                    }
+                </div>
+                <button className='createCourseButton' onClick={() => this.createCourse()}>Create New Class</button>
             </div>
         )
     }
