aaffe183d56bc221bbdf94b9a4de49550db50603
Jordan Buon <jordanmb803@github.com>
2018-05-27 16:16:05 -0600

    Link imported

--- src/components/UpdateCourseRoll/UpdateCourseRoll.js
+++ src/components/UpdateCourseRoll/UpdateCourseRoll.js
@@ -3,6 +3,7 @@ import './UpdateCourseRoll.css';
 import { getUsers } from '../../ducks/user';
 import { connect } from 'react-redux';
 import TAorStudent from '../TAorStudent/TAorStudent';
+import { Link } from 'react-router-dom';
 
 class UpdateCourseRoll extends Component {
     constructor() {
