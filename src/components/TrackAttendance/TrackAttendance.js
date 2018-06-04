28bbe068c37a3153afda6d662e4f8c27e3072fe1
Jordan Buon <jordanmb803@github.com>
2018-06-04 11:12:06 -0600

    styling added

--- src/components/TrackAttendance/TrackAttendance.js
+++ src/components/TrackAttendance/TrackAttendance.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import { connect } from 'react-redux';
 import { getUsers } from '../../ducks/user';
 import DatePicker from 'react-date-picker';
+import '../TrackHours/TrackHours.css';
 
 
 class TrackAttendance extends Component {
@@ -41,35 +42,54 @@ class TrackAttendance extends Component {
     render() {
         console.log(this.state)
         return (
-            <div className='trackAttendance'>
-                <h1>Track Attendance</h1>
-                <select onChange={e => this.setState({ user: e.target.value })}>
-                <option value = '' >Student</option>
-                    {
-                        this.props.users.filter(user => {
-                            return user.type === 'student'
-                        }).map((student, i) => {
-                            return (
-                                <option value={student.user_id} key={student + i}>{student.user_name}</option>
-                            )
-                        })
-                    }
-                </select>
-
-                <DatePicker className='datePicker' value={this.state.dateOne} onChange={e => this.setState({ dateOne: e })} />
-                <DatePicker className='datePicker' value={this.state.dateTwo} onChange={e => this.setState({ dateTwo: e })} />
-
-                    <button onClick={() => this.searchStudent()}>Search</button>
+            <div className='TrackHours'>
+                <h1 className='thHeader'>Track Attendance</h1>
+                <div className='dateSelectorDiv'>
+                    <div className='dateTitleCol'>
+                        <p>From: </p>
+                        <DatePicker className='datePickerTH' value={this.state.dateOne} onChange={e => this.setState({ dateOne: e })} />
+                        <select className='selectTeacher' onChange={e => this.setState({ user: e.target.value })}>
+                            <option value='' >Student</option>
+                            {
+                                this.props.users.filter(user => {
+                                    return user.type === 'student'
+                                }).map((student, i) => {
+                                    return (
+                                        <option value={student.user_id} key={student + i}>{student.user_name}</option>
+                                    )
+                                })
+                            }
+                        </select>
+                    </div>
+                    <div className='dateTitleCol'>
+                        <p>To:</p>
+                        <DatePicker className='datePickerTH' value={this.state.dateTwo} onChange={e => this.setState({ dateTwo: e })} />
+                        <button className='searchButton' onClick={() => this.searchStudent()}>Search</button>
+                    </div>
+                </div>
                 {
                     this.state.hours.map(attendance => {
                         return (
-                            <div key={attendance}>
-                                <p>{attendance.date}</p>
-                                <p>{attendance.user_id}</p>
-                                <p>{attendance.user_name}</p>
-                                <p>{attendance.class_id}</p>
-                                <p>{attendance.title}</p>
-                                <p>{attendance.length}</p>
+                            <div className='classTaughtDiv' key={attendance}>
+                                <p className='date'>{attendance.date}</p>
+                                <div className='classInfoDiv'>
+                                    <div id='info'>
+                                        <p>ID </p>
+                                        <p>{attendance.user_id}</p>
+                                    </div>
+                                    <div id='info'>
+                                        <p>Student </p>
+                                        <p>{attendance.user_name}</p>
+                                    </div>
+                                    <div id='info'>
+                                        <p>Title </p>
+                                        <p>{attendance.title}</p>
+                                    </div>
+                                    <div id='info'>
+                                        <p>Length </p>
+                                        <p>{attendance.length}</p>
+                                    </div>
+                                </div>
                             </div>
                         )
                     })
