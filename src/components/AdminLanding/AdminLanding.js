351758f26aaa7b69d89a73083f9e870dc9c24ee0
Jordan Buon <jordanmb803@github.com>
2018-06-05 11:23:01 -0600

    tab bar onClick added

--- src/components/AdminLanding/AdminLanding.js
+++ src/components/AdminLanding/AdminLanding.js
@@ -1,20 +1,44 @@
 import React, { Component } from 'react';
 import './AdminLanding.css';
-import { Switch, Route, Link} from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 import TrackHours from '../TrackHours/TrackHours';
 import TrackAttendance from '../TrackAttendance/TrackAttendance';
 import Courses from '../Courses/Courses';
 import UsersList from '../UsersList/UsersList';
 
+
 class AdminLanding extends Component {
+    constructor() {
+        super()
+        this.state = {
+            active: 0
+        }
+        this.activeTab = this.activeTab.bind(this)
+    }
+
+    activeTab(tabNum) {
+        this.setState({
+            active: tabNum
+        })
+    }
+
     render() {
+        console.log(this.state.active)
         return (
             <div className='adminLandingRoutes' >
                 <div className='adminLanding'>
-                    <Link className='adminLinks' to='/adminlanding/trackhours'><button className='adminOption'>Hours</button></Link>
-                    <Link className='adminLinks' to='/adminlanding/trackattendance'><button className='adminOption'>Attendance</button></Link>
-                    <Link className='adminLinks' to='/adminlanding/courses'><button className='adminOption'>Classes</button></Link>
-                    <Link className='adminLinks' to='/adminlanding/userslist'><button className='adminOption'>Users</button></Link>
+                    <Link className='adminLinks' id={this.state.active === 0 ? 'active' : null} to='/adminlanding/trackhours'
+                        onClick={() => this.activeTab(0)}
+                    ><h3 className='adminOption'>Hours</h3></Link>
+                    <Link className='adminLinks' id={(this.state.active === 1) ? 'active' : null} to='/adminlanding/trackattendance'
+                        onClick={() => this.activeTab(1)}
+                    ><h3 className='adminOption'>Attendance</h3></Link>
+                    <Link className='adminLinks' id={(this.state.active === 2) ? 'active' : null} to='/adminlanding/courses'
+                        onClick={() => this.activeTab(2)}
+                    ><h3 className='adminOption'>Classes</h3></Link>
+                    <Link className='adminLinks' id={(this.state.active === 3) ? 'active' : null} to='/adminlanding/userslist'
+                        onClick={() => this.activeTab(3)}
+                    ><h3 className='adminOption'>Users</h3></Link>
                 </div>
                 <div>
                     <Switch>
